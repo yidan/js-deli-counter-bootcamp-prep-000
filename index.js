4ca69c270c9f691e7a4e10b227f5e7bdeec63051
Yidan Wang <wangyidan621@gmail.com>
2017-11-02 18:01:09 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+var katzDeli = []
+
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name);
+
+  return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
+  
+}
