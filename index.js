258de09d1126d106e91f878828edbc5e048e0db7
Yidan Wang <wangyidan621@gmail.com>
2017-11-02 18:04:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,13 @@ function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name);
 
   return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
-  
+
+}
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length){
+    return "Currently serving " + katzDeliLine.shift() + "."
+  }
+
+  return "There is nobody waiting to be served!"
 }
